e28a32ace041dade79400fcd91badc8b23db9dd4
adfultz <adfultz1@gmail.com>
2021-11-18 11:11:45 -0600

    added userPoolID, userPoolClientID, region

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_3HFT0DRpn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1r2elffmfeblplk7f8on342p3t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
